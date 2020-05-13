effff770ff589813993c83519eb2d9f468124d7a
Milo Piazza <milopiazza@MacBook-Pro-3.local>
2020-05-13 13:53:50 -0700

    Chapter 5

--- app/models.py
+++ app/models.py
@@ -1,12 +1,20 @@
-from app import db
+from app import db, login
 from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     email = db.Column(db.String(64), unique=True, index=True)
     posts = db.relationship("Post", backref="author", lazy="dynamic")
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
     def __repr__(self):
         return f"User: {self.username}"
@@ -19,3 +27,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return f"Content: {self.content}"
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,15 @@
-from app import app
-from flask import render_template, redirect, url_for, flash
-from app.forms import LoginForm
+from app import app, db
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import login_user, logout_user, current_user, login_required
+from werkzeug.urls import url_parse
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
 
 posts = []
 
 @app.route('/')
 @app.route('/home')
+@login_required
 def home():
     return render_template("home.html.j2", posts=posts)
 
@@ -15,15 +19,36 @@ def about():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form = LoginForm()
     if form.validate_on_submit():
-        flash("Login requested for user {}, remember_me={}".format(
-            form.username.data,
-            form.remember_me.data
-        ))
-        return redirect(url_for("home"))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for("login"))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get("next")
+        if not next_page or url_parse(next_page).netloc != "":
+            return redirect(url_for("home"))
+        return redirect(next_page)
     return render_template("login.html.j2", form=form)
 
-@app.route('/register')
-def register():
+@app.route('/logout')
+def logout():
+    logout_user()
     return redirect(url_for("home"))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        new_user = User(username=form.username.data, email=form.email.data)
+        new_user.set_password(form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        flash("You have successfully registered!")
+        return redirect(url_for("home"))
+    return render_template("register.html.j2", form=form)
